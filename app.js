b27b79597f99f4f025bb56ad657d12b36174ec5e
Andrew Livesay <andrew.livesay@gmail.com>
2015-09-13 19:10:35 +0000

    ...

--- app.js
+++ app.js
@@ -28,9 +28,36 @@ server.register(plugins, function (err) {
 
     server.route({
         method: 'GET',
+        path: '/api/auth',
+        handler: function (request, reply) {
+            ICApi.auth.session({
+                token: request.query.token
+            }, function (err, result) {
+                if (result && result.error && result.error.message === 'already authenticated') {
+                    return reply({ user_id: result.error.user_id});
+                }
+                
+                return reply(err, result);
+            });
+        }
+    });
+    
+    server.route({
+        method: 'GET',
         path: '/api/users/{username}',
         handler: function (request, reply) {
             ICApi.user.detail({
+                token: request.query.token,
+                username: request.params.username
+            }, reply);
+        }
+    });
+    
+        server.route({
+        method: 'GET',
+        path: '/api/users/{username}/stream',
+        handler: function (request, reply) {
+            ICApi.stream.follow({
                 token: request.query.token,
                 username: request.params.username
             }, reply);
--- public/icecompass.js
+++ public/icecompass.js
@@ -1,6 +1,8 @@
 var IceCondor = (function(){
     var exports = {
-        getUser: getUser
+        auth: auth,
+        getUser: getUser,
+        getUserLocation: getUserLocation
     };
     
     function request(options, callback) {
@@ -18,10 +20,26 @@ var IceCondor = (function(){
         });
     }
     
+    function auth(options, callback) {
+        request({
+            method: 'GET',
+            url: '/api/auth',
+            data: { token: options.token },
+        }, callback);
+    }
+    
     function getUser(options, callback) {
         request({
             method: 'GET',
             url: '/api/users/' + options.username,
+            data: { token: options.token },
+        }, callback);
+    }
+    
+    function getUserLocation(options, callback) {
+        request({
+            method: 'GET',
+            url: '/api/users/' + options.username + '/stream',
             data: { token: options.token },
         }, callback);
     }
